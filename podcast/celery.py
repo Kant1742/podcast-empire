9d9eabadf3503f38ad2e63f61a258418784da5cf
Alexander <qazwsxedc19909@gmail.com>
2020-02-20 15:10:48 +0300

    Added requirements.txt and some comments

--- podcast/celery.py
+++ podcast/celery.py
@@ -3,8 +3,15 @@ from celery import Celery
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'podcast.settings')
+
+# For opportunity to use in on Windows.
+# celery -A podcast worker -l info
 os.environ.setdefault('FORKED_BY_MULTIPROCESSING', '1')
+
 app = Celery('podcast')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+# Monitoring via Flower on the address http://localhost:5555/dashboard
+# celery -A podcast flower
