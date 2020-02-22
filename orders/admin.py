6dad12b024481884d2fe0ddd2b2202124e2a54e2
Alexander <qazwsxedc19909@gmail.com>
2020-02-22 17:22:15 +0300

    Export to CSV

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,40 @@
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
 from .models import Order, OrderItem
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;' \
+        f'filename={opts.verbose_name}.csv'
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many
+              and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+    extra = 0
 
 
 @admin.register(Order)
@@ -13,4 +43,7 @@ class OrderAdmin(admin.ModelAdmin):
                     'address', 'postal_code', 'city', 'paid',
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
+    readonly_fields = ('first_name', 'last_name', 'email', 'address',
+                       'postal_code', 'city', 'created', 'updated')
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
